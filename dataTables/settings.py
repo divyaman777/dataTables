3263affd604138b61ff1981310c555c96754a0bb
Your Namedivyaman777 <divyaman777@gmail.com>
2020-01-14 15:16:04 +0530

    LogIn page added!!

--- dataTables/settings.py
+++ dataTables/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'welcome.apps.WelcomeConfig',
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- login/urls.py
+++ login/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'', include('welcome.urls')),
-    path(r'login/', include('login.urls')),
+    path(r'', views.login, name='login'),
 ]
